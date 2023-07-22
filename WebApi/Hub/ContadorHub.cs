8a3dc7e3ca26ff723b120004eec2b4abb5148253
Victor Ferreira <victor.ferreira@inobag.com.br>
2023-07-22 19:15:06 -0300

    receibemessage

--- WebApi/Hub/ContadorHub.cs
+++ WebApi/Hub/ContadorHub.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 using WebApi.RabbitMQ;
 
 namespace WebApi.Hub
@@ -17,8 +16,9 @@ namespace WebApi.Hub
 
         public async Task ReceiveMessage(string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", message);
-        } 
+            var isValidMessage = IsValidMessage(message);
+            await Clients.All.SendAsync("ReceiveMessage", new { content = message, isValid = isValidMessage });
+        }
 
         public async Task SendText(string content)
         {
@@ -30,20 +30,28 @@ namespace WebApi.Hub
             rabbitMqClient.SendMessageToRabbitMq("minha-fila", content);
 
             bool isValidMessage = IsValidMessage(content);
-            await Clients.All.SendAsync("ReceiveMessage", content, isValidMessage);
+            await Clients.All.SendAsync("ReceiveMessage", new { content, isValid = isValidMessage });
         }
 
         private bool IsValidMessage(string content)
         {
+            var listadevalidacoes = new List<string>();
+
             int lowerCaseCount = content.Count(char.IsLower);
             int upperCaseCount = content.Count(char.IsUpper);
             int repeatedCharsCount = content.GroupBy(c => c).Count(g => g.Count() >= 4);
             int specialCharCount = content.Count(c => !char.IsLetterOrDigit(c));
+            if (content.Length < 15 || content.Length > 20) listadevalidacoes.Add("Tamanho inválido");
 
-            return content.Length >= 15 &&
-                   content.Length <= 20 &&
-                   lowerCaseCount >= 2 && upperCaseCount >= 5 &&
-                   repeatedCharsCount >= 4 && specialCharCount >= 2;
+            if (lowerCaseCount < 2) listadevalidacoes.Add("Faltando caracteres mínusculos");
+
+            if (upperCaseCount < 5) listadevalidacoes.Add("Faltando caracteres maiúsculos");
+
+            if (repeatedCharsCount != 1) listadevalidacoes.Add("Faltando caracteres repetidos");
+
+            if (specialCharCount < 2) listadevalidacoes.Add("Faltando caracteres especiais");
+
+            return listadevalidacoes.Count() == 0;
         }
     }
 }
